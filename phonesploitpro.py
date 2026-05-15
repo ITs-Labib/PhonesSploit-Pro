f9a665addf45523248f72f8305debde52c502f73
ITsLabib <87164608+ITs-Labib@users.noreply.github.com>
2026-05-15 12:37:43 -0700

    Add files via upload

---
+++ phonesploitpro.py
@@ -0,0 +1,29 @@
+"""
+    COPYRIGHT DISCLAIMER
+
+    Script : PhoneSploit Pro - All in One Android Hacking ADB Toolkit
+
+    Copyright (C) 2026  ITsLabib (github.com/ITs-Labib)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    Forking and modifying are allowed, but credit must be given to the
+    original developer, [Azeem Idrisi (github.com/AzeemIdrisi)], and copying the code
+    is not permitted without permission.
+"""
+
+from modules.cli import run
+
+if __name__ == "__main__":
+    run()
